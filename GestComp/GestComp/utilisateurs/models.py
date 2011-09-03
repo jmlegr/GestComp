5239e0d5d76eb5f620cde1e0f30e3f7f6887e5f4
Jean-Marc Legrand <jmlegr@gmail.com>
2011-09-03 12:47:10 +0200

    ajout de la procedure de creation d'un eleve

--- GestComp/GestComp/utilisateurs/models.py
+++ GestComp/GestComp/utilisateurs/models.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,Group
 import datetime
 import logging
+from settings import GROUP_ID_ELEVE
 
+def User_Exists(user_name):
+    # teste l'existence d'un utilisateur user_name
+    try:
+        user=User.objects.get(username=user_name)
+    except User.DoesNotExist:
+        return False
+    return True
 
+def supprime_accent(ligne):
+        """ supprime les accents du texte source """
+        accents = { 'a': [u'à', u'ã', u'á', u'â',u'ä'],
+                    'e': [u'é', u'è', u'ê', u'ë'],
+                    'i': [u'î', u'ï'],
+                    'u': [u'ù', u'ü', u'û'],
+                    'o': [u'ô', u'ö'],
+                    'c': [u'ç'] }
+        for (char, accented_chars) in accents.iteritems():
+            for accented_char in accented_chars:
+                ligne = ligne.replace(accented_char, char)
+        return ligne
 
 # TODO: unifier les profils utilisateurs, 
 # (voir http://docs.djangoproject.com/en/dev/topics/auth/#storing-additional-information-about-users )
@@ -53,6 +73,33 @@ class Eleve(models.Model):
             logging.debug("%s entre dans le dispositif" % self.nom)
         super(Eleve,self).save()
         logging.info(u"élève %s, %s sauvegardé" %(self.nom,self.prenom))
+    
+    @classmethod    
+    def create(cls,nom,prenom,**kwargs):
+        '''
+        Création d'un élève et de l'utilisateur associé
+        '''
+        # recherche dun nom d'utilisateur non existant
+        nom_utilisateur=supprime_accent(prenom[0].lower()+nom[:8].lower())
+        if User_Exists(nom_utilisateur):
+            cpt=1
+            while User_Exists(nom_utilisateur+str(cpt)):
+                cpt+=1
+            nom_utilisateur+=str(cpt)
+        # creation de lutilisateur (avec mot de passe idem login)
+        
+        utilisateur=User.objects.create_user(nom_utilisateur,'', nom_utilisateur)  
+        utilisateur.last_name=nom
+        utilisateur.first_name=prenom
+        utilisateur.is_staff=False
+        # TODO: cas aucun Group créé ou mauviase id
+        utilisateur.groups.add(Group.objects.get(id=GROUP_ID_ELEVE))
+        #utilisateur.set_password(utilisateur.username)
+        utilisateur.save()
+        #cretion de l'objet
+        kwargs.update({'nom':nom,'prenom':prenom,'user':utilisateur})
+        obj=cls(**kwargs)
+        obj.save()   
         
 class Groupe(models.Model):
     nom=models.CharField(max_length=30,unique=True)
