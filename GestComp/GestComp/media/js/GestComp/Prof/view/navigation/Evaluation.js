05ea1e7cd34bed2d693149c56a70cf3135bf3b0e
Jean-Marc Legrand <jmlegr@gmail.com>
2011-09-18 16:50:36 +0200

    filtrage sur la date des evaluations dès l'affichage

--- GestComp/GestComp/media/js/GestComp/Prof/view/navigation/Evaluation.js
+++ GestComp/GestComp/media/js/GestComp/Prof/view/navigation/Evaluation.js
@@ -11,7 +11,31 @@ Ext.define('GestComp.Prof.view.navigation.Evaluation',{
 	initComponent: function(){
 		var me=this
 		
-		var tp=new Ext.XTemplate('ff:{name}')
+		//var tp=new Ext.XTemplate('ff:{name}')
+		//calcul de la date de la rentrée pour être large, fixée au 15 aout
+		var today=new Date()
+		var date_rentree=new Date(today.getMonth()>=8?today.getFullYear():today.getFullYear()-1,7,15)
+		var zedate=new Date(2010,1,1)
+		var filters={
+	            ftype: 'filters',
+				//type: 'filters',
+				//encode: true, pour envoie du filtre en json
+				local: true,
+				//autoReload: false,
+				menuFilterText:'Filtre',			
+				//filterCls:'ux-filtered-columnTEST',
+				filters:[{
+		        	   type:'string',
+		        	   dataIndex:'nom',
+		        	   value:'i'
+				},{
+					type:'date',
+					dataIndex:'date_evaluation',
+					 afterText:'Après',
+		             beforeText:'Avant',
+					//value:{after:zedate} --> ne marche pas
+				}]
+	           }
 		this.features= [{
            	ftype:'grouping',
            	startCollapsed:true,
@@ -19,13 +43,7 @@ Ext.define('GestComp.Prof.view.navigation.Evaluation',{
            	//hideGroupedHeader:true,
            	groupHeaderTpl:'{header}: {renderedValue} ({rows.length} évaluation{[values.rows.length > 1 ? "s" : ""]})',           	
            }
-           ,{
-            ftype: 'filters',
-			type: 'filters',
-			encode: true,
-			local: true,
-			autoReload: false,
-           }
+           ,filters
            ];
         this.columns=[
 			{header:'id',dataIndex:'id',hidden:true},
@@ -35,18 +53,49 @@ Ext.define('GestComp.Prof.view.navigation.Evaluation',{
             	falseText: 'Collègue', 
 				filterable:false,			  					
 			},
-			{header:'nom',dataIndex:'nom',filterable:true},
+			{header:'nom',dataIndex:'nom'
+			
+			},			
 			{header:'modification', dataIndex:'date_modification',
-        	 	xtype: 'datecolumn', format: 'd/m/Y', filterable:true,	
+        	 	xtype: 'datecolumn', format: 'd/m/Y',filterable:true,
+        	 	
+        	 	filter:{
+	                type: 'date',
+	                afterText:'Après',
+	                beforeText:'Avant',
+	    			onText : 'Le',
+	            }
         	 },
         	 {header:'date',dataIndex:'date_evaluation',
-        		xtype: 'datecolumn', format: 'd/m/Y',filterable:true
+        		xtype: 'datecolumn', format: 'd/m/Y',filterable:true,
+        		filter:{
+	                type: 'date',
+	                afterText:'Après',
+	                beforeText:'Avant',
+	    			onText : 'Le',
+	            }        		
         	},
         	{header:'Créé par',dataIndex:'user',filterable:true}
         			     
         ]; 
         
         this.callParent() 
+        //console.log('this',this.filters.view.rendered)
+        this.on('afterrender', function(){
+        		/* curieusement, les filtres ne semblent pas exister tant qu'on ne les a pas créés
+        		 * même s'ils sont dans la config et que le menu est là...
+        		 * on recré volontairement un filtre sur la date et on l'active
+        		 */
+        		//Ext.util.Observable.capture(Ext.getCmp('navEvaluations'), console.info)
+        		this.filters.addFilter({
+					  type: 'date',
+		                dataIndex:'date_modification',
+		    			value:{after:zedate}
+		                //value:{after:date_rentree}
+				})
+        		this.filters.getFilter('date_modification').setActive(true)
+        },this,{single:true})
+        
         
         
 	}
